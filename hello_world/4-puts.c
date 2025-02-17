8af204117f276b64c20906adec652526f57e4f1f
ddoudou7 <ddoudou7@yahoo.fr>
2025-02-17 14:32:38 +0000

    Fix Betty style in 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
+ * Description: Prints a message using the puts function.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
