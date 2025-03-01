b7953adf9e521ef59e819dde4033d33e4aac00fb
ddoudou7 <ddoudou7@yahoo.fr>
2025-03-01 22:47:46 +0000

    Fix duplicate declarations in main.h and recompile

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,15 +2,13 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to capitalize
+ * @str: The string to modify
  *
  * Return: Pointer to the modified string
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int capitalize = 1; /* Flag pour savoir si on doit capitaliser */
-
+	int i = 0, capitalize = 1;
 	char separators[] = " \t\n,;.!?\"(){}";
 
 	while (str[i] != '\0')
@@ -21,19 +19,22 @@ char *cap_string(char *str)
 		while (separators[j] != '\0')
 		{
 			if (str[i] == separators[j])
+			{
 				capitalize = 1;
+				break; /* On arrête la boucle car on a trouvé un séparateur */
+			}
 			j++;
 		}
 
-		/* Si on est sur une lettre et qu'on doit capitaliser */
-		if (capitalize && (str[i] >= 'a' && str[i] <= 'z'))
+		/* Si on doit capitaliser une lettre minuscule */
+		if (capitalize && str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] -= 32; /* Convertir en majuscule */
+			str[i] -= 32;
 			capitalize = 0;
 		}
 		else if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			capitalize = 0; /* Si c'est déjà une majuscule, on continue */
+			capitalize = 0; /* Si c'est une majuscule, on continue normalement */
 		}
 
 		i++;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,11 +16,8 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
-void print_rev(char *s);
 char *string_toupper(char *str);
-void rev_string(char *s);
 char *cap_string(char *str);
-void puts2(char *str);
-int _putchar(char c);
 
 #endif /* MAIN_H */
+
