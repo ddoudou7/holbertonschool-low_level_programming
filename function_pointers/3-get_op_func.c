cefaaeea41e3bab136f796a267f34248c2d6758e
ddoudou7 <ddoudou7@yahoo.fr>
2025-03-20 10:52:15 +0000

    Final validation: project fully functional and Betty compliant

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,29 +3,25 @@
 #include <string.h>
 
 /**
-* get_op_func - Sélectionne la bonne fonction en fonction de l'opérateur
-* @s: L'opérateur passé en argument
-*
-* Return: Pointeur vers la fonction correspondant à l'opérateur
-*/
+ * get_op_func - Sélectionne la bonne fonction en fonction de l'opérateur
+ * @s: L'opérateur passé en argument
+ *
+ * Return: Pointeur vers la fonction correspondant à l'opérateur
+ */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
+	op_t ops[] = {
+		{"+", op_add}, {"-", op_sub}, {"*", op_mul},
+		{"/", op_div}, {"%", op_mod}, {NULL, NULL}
+	};
+	int i = 0;
 
-while (ops[i].op != NULL)
-{
-if (ops[i].op[0] == s[0] && !s[1])
-return (ops[i].f);
-i++;
-}
-return (NULL);
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0] && !s[1])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
 
