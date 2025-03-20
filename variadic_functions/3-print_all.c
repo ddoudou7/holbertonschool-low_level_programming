776027df66331926c543f9bcd33bf48efd546154
ddoudou7 <ddoudou7@yahoo.fr>
2025-03-20 14:29:09 +0000

    Fix Task 3: Clean implementation of print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,34 +10,34 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str, *separator = "";
+	char *separator = "", *str;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
-			printf("%s%c", separator, va_arg(args, int));
-		else if (format[i] == 'i')
-			printf("%s%d", separator, va_arg(args, int));
-		else if (format[i] == 'f')
-			printf("%s%f", separator, va_arg(args, double));
-		else if (format[i] == 's')
+		switch (format[i])
 		{
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s%s", separator, str);
-		}
-		else
-		{
-			i++;
-			continue;
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				printf("%s%s", separator, (str ? str : "(nil)"));
+				break;
+			default:
+				i++;
+				continue;
 		}
 		separator = ", ";
 		i++;
 	}
-
 	printf("\n");
 	va_end(args);
 }
