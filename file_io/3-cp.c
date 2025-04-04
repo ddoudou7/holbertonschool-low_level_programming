4113759efe80e414c72b6e5484b84e3f7eecc296
ddoudou7 <ddoudou7@yahoo.fr>
2025-04-04 21:58:09 +0000

    ✅ Refactor: Betty compliant version with <40 lines in main

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,64 +4,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUF_SIZE 1024
+
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, specific exit code otherwise
+ * print_error_and_exit - Print error message and exit with code
+ * @msg: error message format
+ * @arg: string to include
+ * @code: exit code
  */
-int main(int argc, char *argv[])
+void print_error_and_exit(const char *msg, const char *arg, int code)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * copy_file - Copies the content of one file to another
+ * @file_from: source file
+ * @file_to: destination file
+ */
+void copy_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to, r, w;
-	char buf[1024];
+	char buf[BUF_SIZE];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error_and_exit("Error: Can't read from file %s\n", file_from, 98);
 
-	r = read(fd_from, buf, 1024);
+	r = read(fd_from, buf, BUF_SIZE);
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
-		exit(98);
+		print_error_and_exit("Error: Can't read from file %s\n", file_from, 98);
 	}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
-		exit(99);
+		print_error_and_exit("Error: Can't write to %s\n", file_to, 99);
 	}
 
 	while (r > 0)
 	{
 		w = write(fd_to, buf, r);
-		if (w == -1 || w != r)
+		if (w != r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
+			print_error_and_exit("Error: Can't write to %s\n", file_to, 99);
 		}
 
-		r = read(fd_from, buf, 1024);
+		r = read(fd_from, buf, BUF_SIZE);
 		if (r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close(fd_from);
 			close(fd_to);
-			exit(98);
+			print_error_and_exit("Error: Can't read from file %s\n", file_from, 98);
 		}
 	}
 
@@ -70,11 +70,29 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
+
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
 
 	return (0);
 }
