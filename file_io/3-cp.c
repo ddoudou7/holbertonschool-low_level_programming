66ac9f750a05b9a85adfd4bb5aafd151a1675bc8
ddoudou7 <ddoudou7@yahoo.fr>
2025-04-04 21:54:42 +0000

    🛠️ Final fix for checker: correct read() error handling at end

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,16 +4,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUF_SIZE 1024
+
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, exits with code on failure
+ * main - copy content of a file to another
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, exits with specific codes otherwise
  */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, r, w;
-	char buf[1024];
+	char buf[BUF_SIZE];
 
 	if (argc != 3)
 	{
@@ -22,35 +24,24 @@ int main(int argc, char *argv[])
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	if (fd_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+	if (fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
 
-	while (1)
+	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
-		r = read(fd_from, buf, 1024);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close(fd_from);
-			close(fd_to);
-			exit(98);
-		}
-		if (r == 0)
-			break;
-
 		w = write(fd_to, buf, r);
-		if (w == -1 || w != r)
+		if (w != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
@@ -59,15 +50,24 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (close(fd_from) == -1)
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-	if (close(fd_to) == -1)
+	if (close(fd_to) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+
 	return (0);
 }
