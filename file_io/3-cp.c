d1d87c369522fcf4a9042cf4b476f37281a0837b
ddoudou7 <ddoudou7@yahoo.fr>
2025-04-04 22:22:35 +0000

    🚑 Fix: added prototype for transfer_data to avoid implicit declaration

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #define BUF_SIZE 1024
+void transfer_data(int fd_from, int fd_to, const char *file_from, const char *file_to);
 void copy_file(const char *file_from, const char *file_to);
 
 /**
@@ -82,37 +83,13 @@ int main(int argc, char *argv[])
  * @file_from: source file name
  * @file_to: destination file name
  */
-void transfer_data(int fd_from, int fd_to,
-                   const char *file_from, const char *file_to)
-{
-        ssize_t r, w;
-        char buf[1024];
-
-        while ((r = read(fd_from, buf, 1024)) > 0)
-        {
-                w = write(fd_to, buf, r);
-                if (w == -1 || w != r)
-                {
-                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-                        close(fd_from);
-                        close(fd_to);
-                        exit(99);
-                }
-        }
-        if (r == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-                close(fd_from);
-                close(fd_to);
-                exit(98);
-        }
-}
 
 /**
  * copy_file - performs file copy operations
  * @file_from: source file name
  * @file_to: destination file name
  */
+void transfer_data(int fd_from, int fd_to, const char *file_from, const char *file_to);
 void copy_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to;
@@ -139,5 +116,44 @@ void copy_file(const char *file_from, const char *file_to)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
+	}
+}
+
+/**
+ * transfer_data - reads from fd_from, writes to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
+void transfer_data(int fd_from, int fd_to,
+		   const char *file_from, const char *file_to)
+{
+	ssize_t r, w;
+	char buf[1024];
+	ssize_t total_written;
+
+	while ((r = read(fd_from, buf, sizeof(buf))) > 0)
+	{
+		total_written = 0;
+		while (total_written < r)
+		{
+			w = write(fd_to, buf + total_written, r - total_written);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+				close(fd_from);
+				close(fd_to);
+				exit(99);
+			}
+			total_written += w;
+		}
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
 	}
 }
