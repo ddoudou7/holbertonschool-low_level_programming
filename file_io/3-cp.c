31d75334b17424c3627c2533b83d1409b6cced2b
ddoudou7 <ddoudou7@yahoo.fr>
2025-04-04 22:35:11 +0000

    ✅ FINAL FINAL FINAL: main added, checker & Betty OK

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,76 +5,6 @@
 #include <stdio.h>
 
 #define BUF_SIZE 1024
-void transfer_data(int fd_from, int fd_to, const char *file_from, const char *file_to);
-void copy_file(const char *file_from, const char *file_to);
-
-/**
- * open_source_file - opens the source file
- * @file_from: source file name
- * Return: file descriptor
- */
-int open_source_file(const char *file_from)
-{
-	int fd = open(file_from, O_RDONLY);
-
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	return (fd);
-}
-
-/**
- * open_dest_file - opens the destination file
- * @file_to: destination file name
- * @fd_from: fd of source (to close if error)
- * Return: file descriptor
- */
-int open_dest_file(const char *file_to, int fd_from)
-{
-	int fd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(fd_from);
-		exit(99);
-	}
-	return (fd);
-}
-
-/**
- * transfer_data - reads from fd_from, writes to fd_to
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @file_from: source file name
- * @file_to: destination file name
- */
-/**
- * transfer_data - reads from fd_from, writes to fd_to
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @file_from: source file name
- * @file_to: destination file name
- */
-
-/**
- * main - entry point
- * @argc: argument count
- * @argv: argument values
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	copy_file(argv[1], argv[2]);
-	return (0);
-}
 
 /**
  * transfer_data - reads from fd_from, writes to fd_to
@@ -89,22 +19,95 @@ int main(int argc, char *argv[])
  * @file_from: source file name
  * @file_to: destination file name
  */
-void transfer_data(int fd_from, int fd_to, const char *file_from, const char *file_to);
+
+/**
+ * transfer_data - reads from fd_from, writes to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
+
+/**
+ * transfer_data - reads from fd_from, writes to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
+void transfer_data(int fd_from, int fd_to,
+		   const char *file_from, const char *file_to)
+{
+	ssize_t r;
+	char buf[1024];
+
+	while ((r = read(fd_from, buf, sizeof(buf))) > 0)
+	{
+		ssize_t w, total = 0;
+
+		while (total < r)
+		{
+			w = write(fd_to, buf + total, r - total);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+				close(fd_from);
+				close(fd_to);
+				exit(99);
+			}
+			total += w;
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+}
+
+/**
+ * copy_file - orchestrates the copy process using transfer_data
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
+
+/**
+ * copy_file - orchestrates the copy process using transfer_data
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
 void copy_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to;
+	ssize_t test_read;
 	char test_buf[1];
 
-	fd_from = open_source_file(file_from);
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 
-	if (read(fd_from, test_buf, 0) == -1)
+	test_read = read(fd_from, test_buf, 0);
+	if (test_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		close(fd_from);
 		exit(98);
 	}
 
-	fd_to = open_dest_file(file_to, fd_from);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		exit(99);
+	}
+
 	transfer_data(fd_from, fd_to, file_from, file_to);
 
 	if (close(fd_from) == -1)
@@ -120,40 +123,19 @@ void copy_file(const char *file_from, const char *file_to)
 }
 
 /**
- * transfer_data - reads from fd_from, writes to fd_to
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @file_from: source file name
- * @file_to: destination file name
+ * main - Entry point, validates args and calls copy_file
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0 on success, or exits with appropriate code on error
  */
-void transfer_data(int fd_from, int fd_to,
-		   const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
-	ssize_t r, w;
-	char buf[1024];
-	ssize_t total_written;
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-	while ((r = read(fd_from, buf, sizeof(buf))) > 0)
-	{
-		total_written = 0;
-		while (total_written < r)
-		{
-			w = write(fd_to, buf + total_written, r - total_written);
-			if (w == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				close(fd_from);
-				close(fd_to);
-				exit(99);
-			}
-			total_written += w;
-		}
-	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
+	copy_file(argv[1], argv[2]);
+	return (0);
 }
